57a099b33725b8f6f23ba3a63b3c06df9adb2926
buffon11111 <buffonmochammad63@gmail.com>
2023-03-22 23:32:20 +0700

    pertemuan 4

---
+++ pertemuan_4/admin/index.php
@@ -0,0 +1,16 @@
+<?php
+include_once 'templetes/header.php';
+include_once 'templetes/topbar.php';
+include_once 'templetes/sidebar.php';
+?>
+
+<main>
+<div class="container-fluid px-4">
+    <h1 class="mt-4">Selamat Datang di Halaman Admin</h1>
+    
+</div>
+</main>
+
+<?php
+include_once 'templetes/footer.php';
+?>
